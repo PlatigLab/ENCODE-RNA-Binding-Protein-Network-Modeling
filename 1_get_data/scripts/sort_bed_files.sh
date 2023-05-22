068528a9f9351084da46301d2923d51d2900da71
YogiOnBioinformatics <yogi@email.virginia.edu>
2023-05-22 14:14:45 -0400

    bug fix: file suffix mistake

--- 1_get_data/scripts/sort_bed_files.sh
+++ 1_get_data/scripts/sort_bed_files.sh
@@ -4,6 +4,6 @@ files=$(find /project/PlatigLab/RBP/encode/filtered_peaks/raw_unsorted/ -type f)
 
 for file in ${files[@]}
 do 
-    new_file=$(basename $file .bed.gz).sorted.bed.gz
+    new_file=$(basename $file .bed.gz).sorted.bed
     /home/jve4pt/.yogi_utils/bedtools-2.30.0/bedtools sort -i $file > /project/PlatigLab/RBP/encode/filtered_peaks/sorted/$new_file
 done
