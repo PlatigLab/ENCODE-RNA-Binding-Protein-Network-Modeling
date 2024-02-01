d4277c7e1931cece19ea21de542b5750c905d82d
YogiOnBioinformatics <yogi@email.virginia.edu>
2024-02-01 16:36:39 -0500

    remove stale/old things

--- analysis/1_get_data/scripts/sort_bed_files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-files=$(find /project/PlatigLab/RBP/encode/filtered_peaks/raw_unsorted/ -type f)
-
-for file in ${files[@]}
-do 
-    new_file=$(basename $file .bed.gz).sorted.bed
-    /home/jve4pt/.yogi_utils/bedtools-2.30.0/bedtools sort -i $file > /project/PlatigLab/RBP/encode/filtered_peaks/sorted/$new_file
-done
--- miscellaneous_analysis/README.md
+++
@@ -1,5 +0,0 @@
-# Miscellaneous Analysis
-
-📂`aligned_BAM_annotation_comparison/`:
-
-Analysis to understand the differences in genome annotation for alignments between `eCLIP` and `shRNA` data. 
