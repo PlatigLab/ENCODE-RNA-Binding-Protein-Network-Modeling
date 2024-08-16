b70c383f9e60dc383f8456975b430ec877261f80
YogiOnBioinformatics <yogi@email.virginia.edu>
2024-08-16 18:43:01 -0400

    get closest splice junction for each eclip peak per cell line (script + output data)

---
+++ analysis/5_TO_BE_DECIDED/code/3_peaks_to_splice_junctions.sh
@@ -0,0 +1,32 @@
+
+for file in ../output/bedtools_input/*.bed; 
+do
+    echo $file
+    /project/PlatigLab/software/bedtools-v2.31.1/bin/bedtools sort -i $file > ../output/bedtools_input/$(basename $file ".bed")_sorted.bed
+    rm $file
+
+done
+
+
+for file in ../output/bedtools_input/*all_peaks_sorted.bed;
+do 
+
+    echo $file 
+
+    /project/PlatigLab/software/bedtools-v2.31.1/bin/bedtools closest \
+        -a $file \
+        -b ../output/bedtools_input/unique_splicing_junctions_sorted.bed \
+        -s \
+        -D b \
+        -t first > ../output/peaks_to_splice_junctions/$(basename $file "_all_peaks_sorted.bed")_peaks_to_splice_junctions.bed
+
+done
+
+
+
+
+
+
+
+
+
