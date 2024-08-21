1ab1fddf223b5968a1417b52b951c2aa55818eb8
YogiOnBioinformatics <yogi@email.virginia.edu>
2024-08-21 18:30:30 -0400

    change concatenation script to be a job array sbatch script

--- analysis/6_create_RBP_to_PSI_input_ML_datasets/code/3_concatenate_results.sh
+++ analysis/6_create_RBP_to_PSI_input_ML_datasets/code/3_concatenate_results.sh
@@ -1,40 +1,19 @@
 #!/bin/bash
+#SBATCH --account=platiglab    
+#SBATCH --partition=standard    
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --output=../SLURM_output/final_dataset_creation_output_%A_%a.txt
+#SBATCH --error=../SLURM_output/final_dataset_creation_error_%A_%a.txt
+#SBATCH --mem=20GB
+#SBATCH --array=0-139
+
 
 thresholds=(50 100 500 1000 2000 5000 10000)
 cell_lines=(HepG2 K562)
 data_value_variations=("binary-binding-only" "num-peaks-only" "expression-getmm_no-log_dose-dependent-expression" "expression-getmm_no-log_dose-independent-expression" "expression-getmm_yes-log_dose-dependent-expression" "expression-getmm_yes-log_dose-independent-expression" "expression-tmm_no-log_dose-dependent-expression" "expression-tmm_no-log_dose-independent-expression" "expression-tmm_yes-log_dose-dependent-expression" "expression-tmm_yes-log_dose-independent-expression")
 
-
-# # for each cell line
-# for cell_line in "${cell_lines[@]}"
-# do 
-
-#     # Get a list of all gzip'd files matching the cell line
-#     files=($(find ../output/ -type f -name "${cell_line}_*.gz"))
-
-#     # Check if there are any files
-#     if [ ${#files[@]} -eq 0 ]; then
-#         echo "No gzip'd files found in the directory."
-#         exit 1
-#     fi
-
-#     # Uncompress and check the first line of the first file
-#     first_file="${files[0]}"
-#     uncompressed_output=$(gunzip -c "$first_file" | head -n 1)
-
-#     # Loop through the remaining files and compare the first line
-#     for file in "${files[@]:1}"; do
-#         uncompressed_line=$(gunzip -c "$file" | head -n 1)
-#         if [ "$uncompressed_line" != "$uncompressed_output" ]; then
-#             echo "The first line of $file is different from the first line of $first_file."
-#             exit 1
-#         fi
-#     done
-
-# done
-
-# echo "The first line of all uncompressed files is the same."
-
+combo=()
 
 for cell_line in "${cell_lines[@]}"
 do
@@ -45,17 +24,48 @@ do
         for data_value_variation in "${data_value_variations[@]}"
         do
 
-            array=($(find ../output/ -type f  -name "${cell_line}_*_${threshold}_${data_value_variation}.tsv.gz"))
+            combo+=("${cell_line}_*_${threshold}_${data_value_variation}.tsv.gz")
 
-            output_file="../final_modeling_input_datasets/${cell_line}_${threshold}_${data_value_variation}_output.txt"
-            echo "$output_file"
-            # touch "$output_file"
-
-
-            # sbatch --nodes=1 --ntasks=1 --mem=50GB --partition=standard --account=platiglab --output=../SLURM_output/"${cell_line}"_"${rbp}"_"${threshold}".out --error=../SLURM_output/"${cell_line}"_"${rbp}"_"${threshold}".error --wrap="python3 1_script.py --cell_line ${cell_line} --rbp ${rbp} --threshold ${threshold}"
-    
         done 
 
     done
 
 done
+
+# Sort the combo array
+sorted_combo=($(printf '%s\n' "${combo[@]}" | sort))
+
+# Assign the sorted combo array back to the original variable
+combo=("${sorted_combo[@]}")
+
+# Print the first element of the combo array
+find_string="${combo[${SLURM_ARRAY_TASK_ID}]}"
+
+files=($(find ../output/ -name "${find_string}" -type f | sort))
+
+# Replace "*_" with nothing in find_string
+output_file=../final_modeling_input_datasets/$(echo $find_string | sed 's/\*_//g' | sed 's/.tsv.gz/.tsv/g')
+
+# Uncompress the first file in the files array
+gunzip -c "${files[0]}" | head -n1 > "$output_file"
+
+for file in "${files[@]}" 
+do 
+
+    gunzip -c "$file" | tail -n +2 >> "$output_file"
+
+done
+
+
+# Remove rows containing "chrUn_" or "_random"
+sed -i '/chrUn_\|_random/d' "$output_file"
+
+# Remove duplicate rows based on the first column
+awk '!seen[$1]++' "$output_file" > "${output_file}_temp"
+
+# Replace the temporary file with the final output file
+mv "${output_file}_temp" "$output_file"
+
+gzip "$output_file"
+
+
