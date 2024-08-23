e5cf398da80a540e53c05c7856958dfff96d0102
YogiOnBioinformatics <yogi@email.virginia.edu>
2024-08-23 19:58:10 -0400

    only concattenating num peaks as we don't calculate the other matrices anymore

--- analysis/6_create_RBP_to_PSI_input_ML_datasets/code/3_concatenate_results.sh
+++ analysis/6_create_RBP_to_PSI_input_ML_datasets/code/3_concatenate_results.sh
@@ -6,12 +6,15 @@
 #SBATCH --output=../SLURM_output/final_dataset_creation_output_%A_%a.txt
 #SBATCH --error=../SLURM_output/final_dataset_creation_error_%A_%a.txt
 #SBATCH --mem=20GB
-#SBATCH --array=0-139
+#SBATCH --array=0-13
 
 
 thresholds=(50 100 500 1000 2000 5000 10000)
 cell_lines=(HepG2 K562)
-data_value_variations=("binary-binding-only" "num-peaks-only" "expression-getmm_no-log_dose-dependent-expression" "expression-getmm_no-log_dose-independent-expression" "expression-getmm_yes-log_dose-dependent-expression" "expression-getmm_yes-log_dose-independent-expression" "expression-tmm_no-log_dose-dependent-expression" "expression-tmm_no-log_dose-independent-expression" "expression-tmm_yes-log_dose-dependent-expression" "expression-tmm_yes-log_dose-independent-expression")
+# data_value_variations=("binary-binding-only" "num-peaks-only" "expression-getmm_no-log_dose-dependent-expression" "expression-getmm_no-log_dose-independent-expression" "expression-getmm_yes-log_dose-dependent-expression" "expression-getmm_yes-log_dose-independent-expression" "expression-tmm_no-log_dose-dependent-expression" "expression-tmm_no-log_dose-independent-expression" "expression-tmm_yes-log_dose-dependent-expression" "expression-tmm_yes-log_dose-independent-expression")
+
+data_value_variations=("num-peaks-only")
+
 
 combo=()
 
@@ -67,5 +70,3 @@ awk '!seen[$1]++' "$output_file" > "${output_file}_temp"
 mv "${output_file}_temp" "$output_file"
 
 gzip "$output_file"
-
-
