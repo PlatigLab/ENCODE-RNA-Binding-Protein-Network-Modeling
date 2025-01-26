ff388cd1cbc7a3357c7cdd51ec154af6b9686228
YogiOnBioinformatics <yogi@email.virginia.edu>
2025-01-25 23:40:25 -0500

    2 IMPORTANT changes. 1) revert back to previous strategy of each RBP peak goes with only with 1 splice junction. 2) break ties by assigning to all instead of assigning to first

--- analysis/3_create_RBP_ML_input/3_assign_eCLIP_to_splice_junctions/code/3_peaks_to_splice_junctions.sh
+++ analysis/3_create_RBP_ML_input/3_assign_eCLIP_to_splice_junctions/code/3_peaks_to_splice_junctions.sh
@@ -1,32 +1,24 @@
-distances=(25 50 75 100 125 150 175 200 225 250 500 1000 2000 5000 10000)
-
+bedtools=/project/PlatigLab/software/bedtools-v2.31.1/bin/bedtools
 
 for file in ../output/bedtools_input/*.bed; 
 do
     echo $file
-    /project/PlatigLab/software/bedtools-v2.31.1/bin/bedtools sort -i $file > ../output/bedtools_input/$(basename $file ".bed")_sorted.bed
+    $bedtools sort -i $file > ../output/bedtools_input/$(basename $file ".bed")_sorted.bed
     rm $file
 
 done
 
 
-for distance in ${distances[@]};
+for file in ../output/bedtools_input/*all_peaks_sorted.bed;
 do 
 
-    for file in ../output/bedtools_input/*all_peaks_sorted.bed;
-    do 
+    echo $file 
 
-        echo $file 
+    $bedtools closest \
+        -a $file \
+        -b ../output/bedtools_input/unique_splicing_junctions_sorted.bed \
+        -s \
+        -d \
+        -t all > ../output/peaks_to_splice_junctions/$(basename $file "_all_peaks_sorted.bed")_peaks_to_splice_junctions.bed
 
-        output_file = ../output/peaks_to_splice_junctions/$(basename $file "_all_peaks_sorted.bed")_''$distance''_peaks_to_splice_junctions.bed
-
-        /project/PlatigLab/software/bedtools-v2.31.1/bin/bedtools window \
-            -a $file \
-            -b ../output/bedtools_input/unique_splicing_junctions_sorted.bed \
-            -w $distance \
-            -sm > $output_file
-
-        gzip $output_file
-
-    done
 done
