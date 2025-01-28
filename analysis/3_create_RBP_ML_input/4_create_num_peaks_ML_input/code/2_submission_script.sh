98b6b75afdff36b10cbeab621bb9ddd9278b4c87
YogiOnBioinformatics <yogi@email.virginia.edu>
2025-01-28 16:40:13 -0500

    update individual data shard creation submission script to have less thresholds, clean up paths, and only run jobs for RBPs where we have shRNA knockdown data

--- analysis/3_create_RBP_ML_input/4_create_num_peaks_ML_input/code/2_submission_script.sh
+++ analysis/3_create_RBP_ML_input/4_create_num_peaks_ML_input/code/2_submission_script.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
-thresholds=(25 50 75 100 125 150 175 200 225 250 500 1000)
+thresholds=(25 50 75 100 150 200 250 500 1000)
 cell_lines=(HepG2 K562)
+data_path="/project/PlatigLab/data/collaborators/BWH/1_ENCODE_shRNA_RBP_KD_2024-04-hg38-gencode-v29/"
 
 for cell_line in "${cell_lines[@]}"
 do
 
-    rbps=($(cut -f4 /project/PlatigLab/users/yogi/ENCODE-RNA-Binding-Protein-Network-Modeling/analysis/3_create_RBP_ML_input/3_assign_eCLIP_to_splice_junctions/output/bedtools_input/${cell_line}_all_peaks_sorted.bed | cut -d"_" -f1 | sort | uniq))
+    rbps=($(cut -f4 "../../3_assign_eCLIP_to_splice_junctions/output/bedtools_input/${cell_line}_all_peaks_sorted.bed" | cut -d"_" -f1 | sort | uniq))
     
     for rbp in "${rbps[@]}"
     do
 
-        for threshold in "${thresholds[@]}"
-        do
+        if [ -n "$(find ${data_path} -iname "${rbp}-*-${cell_line}" | grep -v "Transfection")" ]; then
 
-            sbatch --nodes=1 --ntasks=1 --mem=50GB --partition=standard --account=platiglab --output=../SLURM_output/"${cell_line}"_"${rbp}"_"${threshold}".out --error=../SLURM_output/"${cell_line}"_"${rbp}"_"${threshold}".error --wrap="python3 ./1_create_cell-line_rbp_dataset.py --cell_line ${cell_line} --rbp ${rbp} --threshold ${threshold}"
-        
-        done
+            for threshold in "${thresholds[@]}"
+            do
+                
+                sbatch --nodes=1 --ntasks=2 --mem=50GB --partition=standard --account=platiglab --output=../SLURM_output/"${cell_line}"_"${rbp}"_"${threshold}".out --error=../SLURM_output/"${cell_line}"_"${rbp}"_"${threshold}".error --wrap="python3 ./1_create_cell-line_rbp_dataset.py --cell_line ${cell_line} --rbp ${rbp} --threshold ${threshold}"
+
+            done
+            
+        else
+            echo "No matching files found for ${cell_line} ${rbp}"
+
+        fi
 
     done
 
