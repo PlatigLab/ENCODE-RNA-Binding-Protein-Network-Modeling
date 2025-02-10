f063969feb7538eb110543d96256c1f4c3b9af5e
YogiOnBioinformatics <yogi@email.virginia.edu>
2025-02-10 11:19:26 -0500

    change file used to get shrna rbps, add data modes, only submit sbatch command if output file doesn't exist

--- analysis/3_create_RBP_ML_input/4_create_num_peaks_ML_input/code/2_submission_script.sh
+++ analysis/3_create_RBP_ML_input/4_create_num_peaks_ML_input/code/2_submission_script.sh
@@ -2,29 +2,44 @@
 
 thresholds=(25 50 75 100 150 200 250 500 1000)
 cell_lines=(HepG2 K562)
+data_modes=("all-events" "non-overlapping")
 data_path="/project/PlatigLab/data/collaborators/BWH/1_ENCODE_shRNA_RBP_KD_2024-04-hg38-gencode-v29/"
 
 for cell_line in "${cell_lines[@]}"
 do
 
-    rbps=($(cut -f4 "../../3_assign_eCLIP_to_splice_junctions/output/bedtools_input/${cell_line}_all_peaks_sorted.bed" | cut -d"_" -f1 | sort | uniq))
-    
+    rbps=($(sort "../../3_assign_eCLIP_to_splice_junctions/output/eclip_and_shrna_rbps/${cell_line}_shrna_rbps.txt"))
+
     for rbp in "${rbps[@]}"
     do
 
-        if [ -n "$(find ${data_path} -iname "${rbp}-*-${cell_line}" | grep -v "Transfection")" ]; then
+        for threshold in "${thresholds[@]}"
+        do
 
-            for threshold in "${thresholds[@]}"
-            do
+            for data_mode in "${data_modes[@]}"
+            do  
+
+                output_file="../output/${cell_line}_${rbp}_${threshold}_${data_mode}_num-peaks-no-kd.tsv.gz"
                 
-                sbatch --nodes=1 --ntasks=2 --mem=50GB --partition=standard --account=platiglab --output=../SLURM_output/"${cell_line}"_"${rbp}"_"${threshold}".out --error=../SLURM_output/"${cell_line}"_"${rbp}"_"${threshold}".error --wrap="python3 ./1_create_cell-line_rbp_dataset.py --cell_line ${cell_line} --rbp ${rbp} --threshold ${threshold}"
+                if [ ! -f "${output_file}" ]; then
+
+                    echo $output_file
+
+                    sbatch \
+                        --nodes=1 \
+                        --ntasks=7 \
+                        --mem=65GB \
+                        --partition=standard \
+                        --account=platiglab \
+                        --output=../SLURM_output/"${cell_line}"_"${rbp}"_"${threshold}"_"${data_mode}".out \
+                        --error=../SLURM_output/"${cell_line}"_"${rbp}"_"${threshold}"_"${data_mode}".error \
+                        --wrap="python3 ./1_create_cell-line_rbp_dataset.py --cell_line ${cell_line} --rbp ${rbp} --threshold ${threshold} --data_mode ${data_mode}"
+                
+                fi
 
             done
-            
-        else
-            echo "No matching files found for ${cell_line} ${rbp}"
 
-        fi
+        done
 
     done
 
