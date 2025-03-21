f6329cb7345b6067f4c0338551aec43a01f56d31
YogiOnBioinformatics <yogi@email.virginia.edu>
2025-03-21 14:46:06 -0400

    add script and yaml file to sweep through the 3 tree construction parameters simultaneously

---
+++ analysis/4_choose_dataset_and_model_parameters/code/2_tree_construction_parameters.py
@@ -0,0 +1,9 @@
+import os
+
+SCRIPT_DIR = "/scratch/jve4pt/platiglib/scripts"
+YAML_FILE = "/project/PlatigLab/users/yogi/ENCODE-RNA-Binding-Protein-Network-Modeling/analysis/4_choose_dataset_and_model_parameters/wandb_configs/model/sequential_sweeps/trees_and_learning.yaml"
+
+os.chdir(SCRIPT_DIR)
+os.system(
+    f'python {SCRIPT_DIR}/send_wandb_sweep.py --memory 256 --cpu 16 --sweep-config {YAML_FILE} --sweep-name "1:n_estimators-learning_rate-max_depth" --max-agents 1000'
+)
