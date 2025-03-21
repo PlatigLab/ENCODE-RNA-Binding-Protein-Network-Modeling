5971ba68c063ce9b19c7e2591c5a65f277530d81
YogiOnBioinformatics <yogi@email.virginia.edu>
2025-03-21 14:55:26 -0400

    rename config yaml

--- analysis/4_choose_dataset_and_model_parameters/code/2_tree_construction_parameters.py
+++ analysis/4_choose_dataset_and_model_parameters/code/2_tree_construction_parameters.py
@@ -1,7 +1,7 @@
 import os
 
 SCRIPT_DIR = "/scratch/jve4pt/platiglib/scripts"
-YAML_FILE = "/project/PlatigLab/users/yogi/ENCODE-RNA-Binding-Protein-Network-Modeling/analysis/4_choose_dataset_and_model_parameters/wandb_configs/model/sequential_sweeps/trees_and_learning.yaml"
+YAML_FILE = "/project/PlatigLab/users/yogi/ENCODE-RNA-Binding-Protein-Network-Modeling/analysis/4_choose_dataset_and_model_parameters/wandb_configs/model/sequential_sweeps/1_trees_and_learning.yaml"
 
 os.chdir(SCRIPT_DIR)
 os.system(
