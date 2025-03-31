dba68e35fb1919f43488d7c2304664be96a01979
YogiOnBioinformatics <yogi@email.virginia.edu>
2025-03-31 19:43:27 -0400

    have file load in the run_ids saved to get the outer loop holdout r2 score values

--- analysis/3_choose_dataset_and_model_parameters/code/7_run_outer_loop_holdout_r2.py
+++ analysis/3_choose_dataset_and_model_parameters/code/7_run_outer_loop_holdout_r2.py
@@ -1,20 +1,17 @@
-import platiglib.wandb_utils
 import os
 
 SCRIPT_DIR = "/scratch/jve4pt/platiglib/scripts"
+OUTPUT_FILE = "../output/chosen_model_hyperparameters/outer_loop_holdout_run_ids.txt"
+PROJECT = "yogi-xgbregressor-hyperparameter-sweep-march-2025"
+
+run_ids = []
+with open(OUTPUT_FILE, "r") as f:
+    for line in f:
+        run_ids.append(line.strip())
+
 os.chdir(SCRIPT_DIR)
-
-project = "yogi-xgbregressor-hyperparameter-sweep-march-2025"
-sweep_id = "f1661ffh"
-
-outer_loop_holdout_r2_sweep_ids = platiglib.wandb_utils.WBProject(
-        project
-    ).sweep_manager.get_runs_in_sweep(
-        sweep_id
-    )
-
-for run_id in outer_loop_holdout_r2_sweep_ids:
+for run_id in run_ids:
     os.system(
-        f"sbatch -J {run_id}_outer_loop_holdout_r2 run_python_script.slurm run_outer_loop_test.py --project {project} --run {run_id}"
+        f"sbatch -J {run_id}_outer_loop_holdout_r2 run_python_script.slurm run_outer_loop_test.py --project {PROJECT} --run {run_id}"
     )
 
