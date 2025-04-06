0e68cf4d83aab757a1f3d1bad1841b56819d6166
YogiOnBioinformatics <yogi@email.virginia.edu>
2025-04-05 20:19:11 -0400

    START ALL OVER AGAIN with hyperparameter tuning except 2 changes: sweep with early_stopping_rounds instead of n_estimators & use default values for hyperparameters that will be sweeped later

--- analysis/3_choose_dataset_and_model_parameters/code/2_tree_construction_parameters.py
+++ analysis/3_choose_dataset_and_model_parameters/code/2_tree_construction_parameters.py
@@ -1,9 +1,11 @@
 import os
 
 SCRIPT_DIR = "/scratch/jve4pt/platiglib/scripts"
-YAML_FILE = "/project/PlatigLab/users/yogi/ENCODE-RNA-Binding-Protein-Network-Modeling/analysis/4_choose_dataset_and_model_parameters/wandb_configs/model/sequential_sweeps/1_trees_and_learning.yaml"
+YAML_FILE = "../wandb_configs/model/2_sequential_sweeps/1_trees_and_learning.yaml"
 
+YAML_ABS_PATH = os.path.abspath(YAML_FILE)
 os.chdir(SCRIPT_DIR)
+
 os.system(
-    f'python {SCRIPT_DIR}/send_wandb_sweep.py --memory 256 --cpu 16 --sweep-config {YAML_FILE} --sweep-name "1:n_estimators-learning_rate-max_depth" --max-agents 1000'
+    f'python {SCRIPT_DIR}/send_wandb_sweep.py --memory 128 --cpu 16 --sweep-config {YAML_ABS_PATH} --sweep-name "1:early_stopping_rounds-learning_rate-max_depth" --max-agents 500'
 )
