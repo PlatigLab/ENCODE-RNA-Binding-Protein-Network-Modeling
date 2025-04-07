29c7ef4b9ef59f38013339029cb7ac9a5412f39b
YogiOnBioinformatics <yogi@email.virginia.edu>
2025-04-07 19:13:33 -0400

    update configs and scripts to run each subsequent sequential sweep in a cell-line specific fashion

--- analysis/3_choose_dataset_and_model_parameters/code/3_tree_splitting_parameters.py
+++ analysis/3_choose_dataset_and_model_parameters/code/3_tree_splitting_parameters.py
@@ -1,9 +1,14 @@
 import os
 
+CELL_LINES = ["K562", "HepG2"]
 SCRIPT_DIR = "/scratch/jve4pt/platiglib/scripts"
-YAML_FILE = "/project/PlatigLab/users/yogi/ENCODE-RNA-Binding-Protein-Network-Modeling/analysis/4_choose_dataset_and_model_parameters/wandb_configs/model/sequential_sweeps/2_tree_splitting_parameters.yaml"
+YAML_FILE_PREFIX= "/project/PlatigLab/users/yogi/ENCODE-RNA-Binding-Protein-Network-Modeling/analysis/3_choose_dataset_and_model_parameters/wandb_configs/model/2_sequential_sweeps/2_tree_splitting_parameters"
 
 os.chdir(SCRIPT_DIR)
-os.system(
-    f'python {SCRIPT_DIR}/send_wandb_sweep.py --memory 128 --cpu 16 --sweep-config {YAML_FILE} --sweep-name "2:min_child_weight-gamma"'
-)
+
+for cell_line in CELL_LINES:
+    YAML_FILE = f"{YAML_FILE_PREFIX}_{cell_line}.yaml"
+    # Run the sweep
+    os.system(
+        f'python {SCRIPT_DIR}/send_wandb_sweep.py --memory 128 --cpu 16 --sweep-config {YAML_FILE} --sweep-name "2:min_child_weight-gamma:{cell_line}"'
+    )
--- analysis/3_choose_dataset_and_model_parameters/code/5_regularization.py
+++ analysis/3_choose_dataset_and_model_parameters/code/5_regularization.py
@@ -1,9 +1,14 @@
 import os
 
+CELL_LINES = ["K562", "HepG2"]
 SCRIPT_DIR = "/scratch/jve4pt/platiglib/scripts"
-YAML_FILE = "/project/PlatigLab/users/yogi/ENCODE-RNA-Binding-Protein-Network-Modeling/analysis/4_choose_dataset_and_model_parameters/wandb_configs/model/sequential_sweeps/4_regularization.yaml"
+YAML_FILE_PREFIX= "/project/PlatigLab/users/yogi/ENCODE-RNA-Binding-Protein-Network-Modeling/analysis/3_choose_dataset_and_model_parameters/wandb_configs/model/2_sequential_sweeps/4_regularization"
 
 os.chdir(SCRIPT_DIR)
-os.system(
-    f'python {SCRIPT_DIR}/send_wandb_sweep.py --memory 128 --cpu 16 --sweep-config {YAML_FILE} --sweep-name "4:reg_lambda-reg_alpha"'
-)
+
+for cell_line in CELL_LINES:
+    YAML_FILE = f"{YAML_FILE_PREFIX}_{cell_line}.yaml"
+    # Run the sweep
+    os.system(
+        f'python {SCRIPT_DIR}/send_wandb_sweep.py --memory 128 --cpu 16 --sweep-config {YAML_FILE} --sweep-name "4:reg_alpha-reg_lambda:{cell_line}"'
+    )
