95c1c7c6ef170028c527c3d3d0c7fe7a303bc00f
YogiOnBioinformatics <yogi@email.virginia.edu>
2025-04-14 13:29:57 -0400

    add OLS model + update elasticnet run script to include OLS

--- analysis/3_choose_dataset_and_model_parameters/code/7_run_elasticnet.py
+++ analysis/3_choose_dataset_and_model_parameters/code/7_run_elasticnet.py
@@ -1,10 +1,16 @@
-import os
+import os, glob
 
 SCRIPT_DIR = "/scratch/jve4pt/platiglib/scripts"
-YAML_FILE = "../wandb_configs/model/3_linear_model_configs/1_elasticnet.yaml"
-YAML_ABS_PATH = os.path.abspath(YAML_FILE)
+YAML_CONFIGS = [os.path.abspath(yaml_file) for yaml_file in glob.glob("../wandb_configs/model/3_linear_model_configs/*.yaml")]
 os.chdir(SCRIPT_DIR)
 
-os.system(
-    f'python {SCRIPT_DIR}/send_wandb_sweep.py --memory 128 --cpu 16 --sweep-config {YAML_ABS_PATH} --sweep-name "ElasticNet" --max-agents 150 --time 12:00:00'
-)
+for file in YAML_CONFIGS:
+
+    if "elasticnet" in file.lower(): 
+        sweep_name = "ElasticNet"
+    elif "ols" in file.lower(): 
+        sweep_name = "OLS"
+
+    os.system(
+        f'python {SCRIPT_DIR}/send_wandb_sweep.py --memory 128 --cpu 16 --sweep-config {file} --sweep-name {sweep_name}'
+    )
