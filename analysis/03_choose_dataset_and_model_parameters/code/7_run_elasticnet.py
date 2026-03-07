0c892e4e64a750251d94192b5144a0f4af2acc59
YogiOnBioinformatics <yogi@email.virginia.edu>
2026-03-06 23:52:06 -0500

    add elasticnet sweep config updates

--- analysis/03_choose_dataset_and_model_parameters/code/7_run_elasticnet.py
+++ analysis/03_choose_dataset_and_model_parameters/code/7_run_elasticnet.py
@@ -1,16 +1,19 @@
 import os, glob
 
 SCRIPT_DIR = "/scratch/jve4pt/platiglib/scripts"
-YAML_CONFIGS = [os.path.abspath(yaml_file) for yaml_file in glob.glob("../wandb_configs/model/3_linear_model_configs/*.yaml")]
+YAML_CONFIG = os.path.abspath(
+    "../wandb_configs/model/3_linear_model_configs/1_elasticnet.yaml"
+)
+
 os.chdir(SCRIPT_DIR)
 
-for file in YAML_CONFIGS:
 
-    if "elasticnet" in file.lower(): 
-        sweep_name = "ElasticNet"
-    elif "ols" in file.lower(): 
-        sweep_name = "OLS"
-
-    os.system(
-        f'python {SCRIPT_DIR}/send_wandb_sweep.py --memory 128 --cpu 16 --sweep-config {file} --sweep-name {sweep_name}'
-    )
+os.system(
+    f'python3.11 {SCRIPT_DIR}/send_wandb_sweep.py '
+    '--memory 128 '
+    '--cpu 16 '
+    f'--sweep-config {YAML_CONFIG} '
+    '--time=08:00:00 '
+    '--account=platiglab_paid '
+    '--max-agents 100'
+)
