41798ca7ccba094db85558b9cc4743b8de8c1456
ReeceAllyson_Anderson <bhd5qr@virginia.edu>
2026-05-08 12:04:17 -0400

    re-did shap analysis without regenerating shap

---
+++ analysis/07_reece_biological_validations/1_for_figures/4_Crispr_CTRL_SHAP/K562/preprocess_K562.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=precompute_bat
+#SBATCH --output=logs/precompute.out
+#SBATCH --error=logs/precompute.err
+#SBATCH --account=PlatigLab
+#SBATCH --partition=standard
+#SBATCH --time=01:00:00
+#SBATCH -n 4
+#SBATCH --mem=64G   # just enough for the BAT
+
+python3.11 K562_precompute_rmats_matches.py
